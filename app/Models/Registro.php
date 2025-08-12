a8feba5b6adf809cddc1373b74c3ad00c10a3ac6
maartins-dudaa <mmfmecma203@gmail.com>
2025-08-12 15:45:11 -0300

    fazendo model e migrate

---
+++ app/Models/Registro.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Registro extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'sensor_id',
+        'valor',
+        'unidade',
+        'data_hora'
+    ];
+
+    protected $cast = [
+        'data_hora' => 'datetime'
+    ];
+
+    public function sensor(){
+        return $this->belongsTo(Sensor::class);
+    }
+}
+
