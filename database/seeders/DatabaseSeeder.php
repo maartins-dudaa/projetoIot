93029bba4eeb1589188296ecb1106a353d9d0c78
maartins-dudaa <mmfmecma203@gmail.com>
2025-08-21 14:07:07 -0300

    Criando dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AmbienteSeeder::class,
-            SensorSeeder::class
+            SensorSeeder::class,
+            RegistroSeeder::class
         ]);
     }
 }
